05f27878d19b4e2de5f4d53debb69e722b321e4e
Jacob160-oss <reinholdkjacob@gmail.com>
2025-02-23 10:20:32 +0000

    First Commit

---
+++ includes/footer.php
@@ -0,0 +1,5 @@
+<footer>
+        <div class="footer-area">
+                <p>© <?php echo date("Y");?> | GPMS Leave Management System| Developed By <a href="https://www.gpmsonline.com/">GPMS</a></p>
+        </div>
+</footer>
